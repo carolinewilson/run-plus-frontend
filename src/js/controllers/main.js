35f8c1e94f42b36ed0cc7c103e3feea58691a95c
carolinewilson <carolinecwilson3+github@gmail.com>
2016-12-12 13:04:41 +0000

    Adding today to nav bar

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -1,16 +1,39 @@
 angular.module('finalProject')
   .controller('MainController', MainController);
 
-MainController.$inject = ['$auth','$state'];
-function MainController($auth, $state){
+MainController.$inject = ['$auth','$state','User','UserPlan', '$window'];
+function MainController($auth, $state, User, UserPlan, $window){
   const main = this;
+  const moment = $window.moment;
 
   main.isLoggedIn = $auth.isAuthenticated;
+  main.hasActivePlan = false;
+  main.currentUser = $auth.getPayload().id;
 
   if (main.isLoggedIn()) {
     main.currentUser = $auth.getPayload().id;
   }
-  
+
+  main.all = User.get({id: main.currentUser}, (res) => {
+    res.user_plans.forEach((plan) => {
+      if (plan.active) {
+        main.activePlan = plan.id;
+        
+        UserPlan.get({id: plan.id}, (data) => {
+          data.user_days.forEach((day) => {
+            const date = moment(day.date).format('YYYY-MM-DD');
+            const today = moment().format('YYYY-MM-DD');
+            if (date === today) {
+              main.hasActivePlan = true;
+              main.dayId = day.id;
+            }
+          });
+        });
+      }
+    });
+  });
+
+
   function logout() {
     $auth.logout()
       .then(() => {
