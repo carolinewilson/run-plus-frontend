11c3c389000234dbc63bb7b37da14e1ae7553b58
carolinewilson <carolinecwilson3+github@gmail.com>
2016-12-12 14:00:36 +0000

    Updating homepage and nav bar to conditionaly show new plans and today

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -8,30 +8,29 @@ function MainController($auth, $state, User, UserPlan, $window){
 
   main.isLoggedIn = $auth.isAuthenticated;
   main.hasActivePlan = false;
-  main.currentUser = $auth.getPayload().id;
 
   if (main.isLoggedIn()) {
     main.currentUser = $auth.getPayload().id;
-  }
 
-  main.all = User.get({id: main.currentUser}, (res) => {
-    res.user_plans.forEach((plan) => {
-      if (plan.active) {
-        main.activePlan = plan.id;
-        
-        UserPlan.get({id: plan.id}, (data) => {
-          data.user_days.forEach((day) => {
-            const date = moment(day.date).format('YYYY-MM-DD');
-            const today = moment().format('YYYY-MM-DD');
-            if (date === today) {
-              main.hasActivePlan = true;
-              main.dayId = day.id;
-            }
+    main.all = User.get({id: main.currentUser}, (res) => {
+      res.user_plans.forEach((plan) => {
+        if (plan.active) {
+          main.activePlan = plan.id;
+
+          UserPlan.get({id: plan.id}, (data) => {
+            data.user_days.forEach((day) => {
+              const date = moment(day.date).format('YYYY-MM-DD');
+              const today = moment().format('YYYY-MM-DD');
+              if (date === today) {
+                main.hasActivePlan = true;
+                main.dayId = day.id;
+              }
+            });
           });
-        });
-      }
+        }
+      });
     });
-  });
+  }
 
 
   function logout() {
