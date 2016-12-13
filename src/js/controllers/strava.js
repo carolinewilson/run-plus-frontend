17b918647201fb6a6ec8e7b735c4f61d84b36309
carolinewilson <carolinecwilson3+github@gmail.com>
2016-12-13 15:58:07 +0000

    Adding buttons to strava feed that links to dayShow

--- src/js/controllers/strava.js
+++ src/js/controllers/strava.js
@@ -1,21 +1,22 @@
 angular.module('finalProject')
   .controller('StravaIndexController', StravaIndexController);
 
-StravaIndexController.$inject = ['$http', 'StravaService','$auth', 'User','UserPlan'];
-function StravaIndexController($http, StravaService, $auth, User, UserPlan){
+StravaIndexController.$inject = ['$http', 'StravaService','$auth', 'User','UserPlan', '$window'];
+function StravaIndexController($http, StravaService, $auth, User, UserPlan, $window){
   const stravaIndex = this;
+  const moment = $window.moment;
   const userId = $auth.getPayload().id;
 
   User.get({id: userId}, (res) => {
     res.user_plans.forEach((plan) => {
       if (plan.active) {
+        stravaIndex.userPlanId = plan.id;
         UserPlan.get({id: plan.id }, (data) => {
           stravaIndex.userDays = data.user_days;
           data.user_days.forEach((day) => {
-            day.date = moment(day.date).format("YYYY-MM-DD");
+            day.date = moment(day.date).format('YYYY-MM-DD');
           });
 
-          console.log(data.user_days);
         });
       }
     });
@@ -29,7 +30,6 @@ function StravaIndexController($http, StravaService, $auth, User, UserPlan){
     .getActivities(accessToken)
     .then(
       successResponse => {
-        // console.log(successResponse);
         stravaIndex.data = successResponse;
       },
       errorResponse => {
@@ -37,4 +37,9 @@ function StravaIndexController($http, StravaService, $auth, User, UserPlan){
       }
     );
 
+  function viewDay(stravaActivityId) {
+    console.log('clicked');
+    console.log(stravaActivityId);
+  }
+  stravaIndex.viewDay = viewDay;
 }
