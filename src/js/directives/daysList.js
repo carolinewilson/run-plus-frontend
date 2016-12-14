32a87aecaed04d493824aca2763e45c87d0bca15
carolinewilson <carolinecwilson3+github@gmail.com>
2016-12-14 11:54:52 +0000

    Adding listShow directive for week show and full plan

---
+++ src/js/directives/daysList.js
@@ -0,0 +1,17 @@
+angular.module('finalProject')
+  .directive('daysList', daysList);
+
+function daysList() {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'templates/daysList.html',
+    scope: {
+      days: '=',
+      plan: '='
+    },
+    link: function($scope) {
+      console.log($scope);
+    }
+  };
+}
