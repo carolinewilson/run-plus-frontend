ba652a80db97c42624e1de8d144d1de4fc95840e
carolinewilson <carolinecwilson3+github@gmail.com>
2016-12-14 09:57:13 +0000

    Adding google map with polylines

--- src/js/directives/googleMap.js
+++ src/js/directives/googleMap.js
@@ -8,12 +8,24 @@ function googleMap($window) {
     replace: true,
     template: '<div class="google-map"></div>',
     scope: {
-      center: '='
+      data: '='
     },
     link: function($scope, element) {
-      const map = new $window.google.maps.Map(element[0], {
-        center: {lat: 51, lng: -1},
-        zoom: 12
+      $scope.$watch('data', () => {
+        const map = new   $window.google.maps.Map(element[0], {
+          center: {lat: $scope.data.map.polyline[0].lat, lng: $scope.data.map.polyline[0].lng},
+          zoom: 14
+        });
+        
+        const run = new $window.google.maps.Polyline({
+          path: $scope.data.map.polyline,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+
+        run.setMap(map);
       });
     }
   };
